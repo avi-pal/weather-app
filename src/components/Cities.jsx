817b4d0eb1b9cb60595c39732534a24e1fe40f2a
avi-pal <avirup150@gmail.com>
2024-04-10 23:02:30 +0530

    added country timezone to cities table and sort feat

--- src/components/Cities.jsx
+++ src/components/Cities.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { FaArrowLeftLong, FaArrowRightLong } from "react-icons/fa6";
+import { LiaSortSolid } from "react-icons/lia";
 
 const Cities = () => {
   const navigate = useNavigate();
@@ -9,11 +10,15 @@ const Cities = () => {
   localStorage.setItem("offset", 0);
   const [city, setCity] = useState([]);
   const [loading, setLoading] = useState(true);
-  const [isNearEnd, setIsNearEnd] = useState(false);
+  const [isNearEnd, setIsNearEnd] = useState(true);
+  const [query, setQuery] = useState("");
+  let [citySort, setCitySort] = useState("desc");
+  let [countrySort, setCountrySort] = useState("desc");
+  let [timeZoneSort, setTimeZoneSort] = useState("desc");
   const getCities = async () => {
     try {
       const rawData = await axios.get(
-        `https://public.opendatasoft.com/api/explore/v2.1/catalog/datasets/geonames-all-cities-with-a-population-1000/records?limit=20&offset=${localStorage.getItem(
+        `https://public.opendatasoft.com/api/explore/v2.1/catalog/datasets/geonames-all-cities-with-a-population-1000/records?limit=20${query}&offset=${localStorage.getItem(
           "offset"
         )}`
       );
@@ -25,13 +30,14 @@ const Cities = () => {
       console.log(data);
       setCity([...city, ...data]);
       setLoading(false);
+      console.log("here");
     } catch (e) {
       alert(e.message);
     }
   };
   useEffect(() => {
     getCities();
-  }, [loading, isNearEnd]);
+  }, [loading, isNearEnd, query]);
   useEffect(() => {
     const handleScroll = () => {
       const { scrollTop, clientHeight, scrollHeight } =
@@ -49,6 +55,58 @@ const Cities = () => {
   }, []);
   return (
     <>
+      <div className="grid grid-cols-3 justify-between bg-[#202B3B] rounded p-3 my-5">
+        <span
+          className="text-white text-sm font-comforta flex items-center"
+          onClick={() => {
+            setCity([]);
+            localStorage.setItem("offset", 0);
+            if (citySort === "desc") {
+              setCitySort("asc");
+              setQuery("&order_by=name%20asc");
+            } else {
+              setCitySort("desc");
+              setQuery("&order_by=name%20desc");
+            }
+          }}
+        >
+          City <LiaSortSolid />
+        </span>
+        <span
+          className="text-white text-sm font-comforta flex items-center "
+          onClick={() => {
+            setCity([]);
+            localStorage.setItem("offset", 0);
+            if (countrySort === "desc") {
+              setCountrySort("asc");
+              setQuery("&order_by=cou_name_en%20asc");
+            } else {
+              setCountrySort("desc");
+              setQuery("&order_by=cou_name_en%20desc");
+            }
+          }}
+        >
+          Country
+          <LiaSortSolid />
+        </span>
+        <span
+          className="text-white text-sm font-comforta flex items-center justify-end"
+          onClick={() => {
+            setCity([]);
+            localStorage.setItem("offset", 0);
+            if (timeZoneSort === "desc") {
+              setTimeZoneSort("asc");
+              setQuery("&order_by=timezone%20asc");
+            } else {
+              setTimeZoneSort("desc");
+              setQuery("&order_by=timezone%20desc");
+            }
+          }}
+        >
+          Timezone
+          <LiaSortSolid />
+        </span>
+      </div>
       {loading
         ? ""
         : city.map((city, index) => {
@@ -60,44 +118,20 @@ const Cities = () => {
                   navigate("/", { state: { loc: city.name } });
                 }}
               >
-                <h3 className="font-comforta text-white font-bold">
-                  {city.name}
-                </h3>
+                <div className="grid grid-cols-3 w-full justify-between">
+                  <div className="font-comforta text-white text-sm font-bold text-nowrap">
+                    {city.name.slice(0, 10)}
+                  </div>
+                  <div className="font-comforta text-white text-sm font-bold">
+                    {city.cou_name_en.split(",")[0]}
+                  </div>
+                  <div className="font-comforta text-white text-sm font-bold text-end">
+                    {city.timezone.split("/")[0]}
+                  </div>
+                </div>
               </div>
             );
           })}
-      {/* <div className="flex justify-center gap-5">
-        <div
-          className={`${
-            localStorage.getItem("offset") >= 20
-              ? "bg-[#202B3B]"
-              : "bg-[#111b29]"
-          } rounded p-4 px-12 cursor-pointer`}
-          onClick={() => {
-            if (localStorage.getItem("offset") >= 20) {
-              localStorage.setItem(
-                "offset",
-                JSON.stringify(JSON.parse(localStorage.getItem("offset")) - 20)
-              );
-              setLoading(true);
-            }
-          }}
-        >
-          <FaArrowLeftLong color="white" />
-        </div>
-        <div
-          className="bg-[#202B3B] rounded p-4 px-12 cursor-pointer"
-          onClick={() => {
-            localStorage.setItem(
-              "offset",
-              JSON.stringify(JSON.parse(localStorage.getItem("offset")) + 20)
-            );
-            setLoading(true);
-          }}
-        >
-          <FaArrowRightLong color="white" />
-        </div>
-      </div> */}
     </>
   );
 };
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -111,13 +111,13 @@ const Input = ({ search }) => {
         }}
       />
       <AiOutlineEnter className="absolute right-4 bottom-3" color="white" />
-      <FaLocationCrosshairs
+      {/* <FaLocationCrosshairs
         className="absolute right-9 bottom-3 cursor-pointer"
         color="white"
         onClick={() => {
           navigate("/");
         }}
-      />
+      /> */}
       <div className="absolute top-full bg-gray-500">
         {cities.map((city, index) => {
           return (
